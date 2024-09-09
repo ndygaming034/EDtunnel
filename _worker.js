00fd003a15d571627c35421259ce56f9354dddde
ndygaming034 <159202237+ndygaming034@users.noreply.github.com>
2024-09-10 05:55:51 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@
 import { connect } from "cloudflare:sockets";
  
 let Pswd = "ndi";
-const proxyIPs = ["8.222.128.32"]; //ts.hpc.tw edgetunnel.anycast.eu.org bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org proxy.xxxxxxxx.tk
+const proxyIPs = ["8.222.128.32"]
 let cn_hostnames = [''];
 let CDNIP = 'www.visa.com.sg'
 // http_ip
