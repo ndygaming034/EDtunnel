45185b7e24166d7d16872501d54eb9d56aa3ff9f
ndygaming034 <159202237+ndygaming034@users.noreply.github.com>
2024-09-22 07:14:16 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '37a61509-591d-413a-8b12-e1be6b1ce10b';
 
-const พร็อกซีไอพีs = ['3.78.125.180'];
+const พร็อกซีไอพีs = ['188.166.255.195'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
